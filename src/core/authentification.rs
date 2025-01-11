1f323720012e9201199d5676d59f2147eeb89724
pavle <vlajkovicpavle123@gmail.com>
2025-01-11 09:46:23 +0100

    Basic cli inputs implemented
    - Login
    - Save
    - Yank
    - List

--- src/core/authentification.rs
+++ src/core/authentification.rs
@@ -1,11 +1,3 @@
-pub fn authentificate_user(save_login: bool) {
-    let user_password: String = fetch_password_from_cli();
-    println!("The password is {}", user_password);
-}
-
-fn fetch_password_from_cli() -> String {
-    let password = rpassword::read_password().unwrap();
-    // TODO
-    // Do some password validation!
-    password
+pub fn authentificate_user(save_login: bool, password: &String) {
+    println!("The password is {}", password);
 }
--- src/ui/cli.rs
+++ src/ui/cli.rs
@@ -11,18 +11,46 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    /// Initiate login | Add -s if you want to save login
+    #[command(about = "Initiate login")]
     Login {
-        #[arg(short = 's')]
+        #[arg(short = 's', long = "save", help = "Save the session")]
         save: bool,
     },
+    #[command(about = "Save credentials")]
+    Save {
+        #[arg(short = 'l', long = "label", help = "Credential label")]
+        label: String,
+        #[arg(short = 'u', long = "username", help = "Credential username")]
+        username: String,
+    },
+    #[command(about = "Yank credentials by label")]
+    Yank {
+        #[arg(
+            short = 'l',
+            long = "label",
+            help = "Label that identifies credentials"
+        )]
+        label: String,
+        #[arg(
+            short = 'c',
+            long = "clipboard",
+            help = "Save credentials to clipboard"
+        )]
+        clipboard: bool,
+    },
+    #[command(about = "List all credential labels")]
+    List {},
 }
 
 pub fn run_cli() {
     let arguments = Cli::parse();
     match arguments.command {
         Commands::Login { save } => {
-            authentificate_user(save);
+            let user_password = rpassword::prompt_password("Your password: ").unwrap();
+            authentificate_user(save, &user_password);
         }
+        Commands::Yank { label, clipboard } => {}
+        Commands::List {} => {}
+        Commands::Save { label, username } => {}
     }
 }
