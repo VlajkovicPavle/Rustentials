0f1a77dc5b57f8dfb41cfa00ec48c10cc5de1151
mirjana9 <mirjanajocovic2@gmail.com>
2025-01-13 19:17:23 +0100

    Implemented inserting credentials into database
    - Added new model for credential

---
+++ src/models/credential.rs
@@ -0,0 +1,5 @@
+pub struct Credential {
+    pub username: String,
+    pub encrypted_password: String,
+    pub service_name: String,
+}
--- src/repository/queries/insert_credentials.rs
+++ src/repository/queries/insert_credentials.rs
@@ -1,8 +1,10 @@
 fn fetch_insert_credentials_query() -> String {
     String::from(
         r#"
-            INSERT INTO credentials (service_name, username, password)
-            VALUES (?, ?, ?)
+            INSERT INTO credentials (user_id, username, password_hash, service_name)
+            VALUES (
+                (SELECT id FROM users WHERE username = ?),  ?,  ?, ? 
+            )
         "#,
     )
 }
