5a5af15b7c381134b35abedcd1980abe6f1ab804
PavleVlajkovic <pavlev@comtrade.com>
2025-01-14 11:47:14 +0100

    Wrote db services tests

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,8 +1,12 @@
-use rustentials::core::authentification::*;
 #[cfg(test)]
 use rustentials::core::authentification::{
     generate_master_password_hash, validate_password, verify_master_password,
 };
+use rustentials::models::user::User;
+use rustentials::repository::db::{create_database, fetch_db_instances};
+use rustentials::repository::services::{fetch_user_password_hash, insert_user};
+
+// Authentification tests
 
 #[test]
 fn password_validator_test() {
@@ -36,4 +40,39 @@ fn master_password_verify() {
         &master_password_hash,
         master_password
     ));
+}
+
+// Database querries tests
+#[async_std::test]
+async fn test_database_creation() {
+    // Successful creation of database
+    assert!(create_database("sqlite://test.db").await);
+    // No creation if it exists
+    assert!(!create_database("sqlite://test.db").await);
+}
+
+#[async_std::test]
+async fn test_fetching_app_db_instance() {
+    assert!(fetch_db_instances().await.is_ok());
+}
+
+#[async_std::test]
+async fn test_inserting_user() {
+    let test_user: User = User {
+        username: String::from("pavle"),
+        password_hash: String::from("asdasdd"),
+    };
+    assert!(insert_user(&test_user).await);
+}
+
+#[async_std::test]
+async fn test_fething_user_password() {
+    let test_user: User = User {
+        username: String::from("luka"),
+        password_hash: String::from("asdasdd"),
+    };
+    assert!(insert_user(&test_user).await);
+    assert!(fetch_user_password_hash(&test_user.username)
+        .await
+        .is_some());
 }
